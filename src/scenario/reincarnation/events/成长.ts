77d3674c3723f5390a7d5744225fe8491516d05f
vivapercuore <vivapercuore@aliyun.com>
2021-12-06 17:32:08 +0800

    feat: 自动步进版本号

---
+++ src/scenario/reincarnation/events/成长.ts
@@ -0,0 +1,19 @@
+
+const events = {
+    eventPackName: "人生",
+    events: [
+        {
+            name: "平平无奇的生活", //名称,其他事件会通过这个名称跳转过来
+            text: "平安无事的度过了一年", //出现的生活显示的文字
+            options: [
+                {
+                    label: "新年快乐",
+                    // choosenText: "平安无事的度过了一年", //没有的话会显示上面那个text
+                    goto: [], //另一个事件的 name 列表,随机显示其中一个,如果没有,就会随机选择下一个符合的事件,用于事件链
+                },
+            ]
+        },
+    ]
+}
+
+export default events
